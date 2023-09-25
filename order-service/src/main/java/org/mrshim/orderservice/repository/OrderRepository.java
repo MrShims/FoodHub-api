ce8fb6ffd037a5e622dee9043a56f23dcb87eecc
MrShims <79413656+MrShims@users.noreply.github.com>
2023-09-25 21:29:49 +0300

    add png

--- order-service/src/main/java/org/mrshim/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/org/mrshim/orderservice/repository/OrderRepository.java
@@ -2,9 +2,11 @@ package org.mrshim.orderservice.repository;
 
 import org.mrshim.orderservice.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
 
